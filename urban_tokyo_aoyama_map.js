86dfd600e78012ca235fbc37456dcc4558768e7b
ixxiiris <ucfnxze@ucl.ac.uk>
2025-05-13 19:33:40 +0100

    哈哈

--- urban_tokyo_aoyama_map.js
+++ urban_tokyo_aoyama_map.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaXh4aWlyaXMiLCJhIjoiY202aTB2bTI1MDNpNTJqc2h0N
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/ixxiiris/cm8lrey81005d01s9byfx4474',
-  center: [139.714, 35.669], // Aoyama center
+  center: [139.710, 35.666], // Aoyama center
   zoom: 15
 });
 
